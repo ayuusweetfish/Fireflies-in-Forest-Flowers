6f66ab185696a10a73333d26421f756c96db0c73
Shiqing <shiqing-thu18@yandex.com>
2022-01-26 17:36:33 +0800

    Refactor; fireflies as separate entities

--- scene_game.cc
+++ scene_game.cc
@@ -44,25 +44,21 @@ public:
     return (vec2(x, y) - vec2(OFFSET_X, OFFSET_Y)) / SCALE;
   }
 
-  // ==== Fireflies ====
-  struct firefly {
+  // ==== Tracks ====
+  struct track {
     vec2 o;
-    float T;  // Period
+    float len;  // Total length in units
 
-    float tc;       // Current phase
-    float tc0;      // Phase at start
-    bool selpath;   // Selected path
-    bool selpt;     // Selected lighting point
+    bool sel;   // Is selected
 
-    firefly() { }
-    firefly(vec2 o, float T, float tc)
-      : o(o), T(T), tc(tc),
-        selpath(false), selpt(false)
+    track() { }
+    track(vec2 o, float len)
+      : o(o), len(len),
+        sel(false)
       { }
     // Local position at given phase
     virtual vec2 local_at(float t) const = 0;
     vec2 at(float t) const { return local_at(t) + o; }
-    vec2 at() const { return at(tc); }
     // Phase of nearest point on path, given a local point
     // Returns <phase, distance>
     virtual std::pair<float, float> local_nearest(vec2 p) const = 0;
@@ -71,23 +67,46 @@ public:
     virtual void draw() const = 0;
   };
 
-  struct firefly_cir : public firefly {
-    float r, v; // Radius and velocity
-    firefly_cir(vec2 o, float r, float v, float tc)
-      : r(r), v(v),
-        firefly(o, 2 * M_PI * r / v, tc)
+  struct track_cir : public track {
+    float r;  // Radius
+    track_cir(vec2 o, float r)
+      : r(r),
+        track(o, 2 * M_PI * r)
       { }
-    vec2 local_at(float t) const { return vec2(r, 0).rot(t * (2 * M_PI)); }
+    vec2 local_at(float t) const { return vec2(r, 0).rot(t / r); }
     std::pair<float, float> local_nearest(vec2 p) const {
       float a = atan2f(p.y, p.x);
       if (a < 0) a += 2 * M_PI;
-      return {a / (2 * M_PI), (p - vec2(r, 0).rot(a)).norm()};
+      return {a * r, (p - vec2(r, 0).rot(a)).norm()};
     }
     void draw() const {
       using namespace rl;
       // DrawCircleV(scr(o), 3, DARKGRAY);
-      DrawRing(scr(o), r * SCALE - 2, r * SCALE + 2, 0, 360, 48, selpath ? RED : WHITE);
-      DrawCircleV(scr(at()), 4, selpt ? RED : YELLOW);
+      DrawRing(scr(o), r * SCALE - 2, r * SCALE + 2, 0, 360, 48, sel ? RED : WHITE);
+    }
+  };
+
+  // ==== Fireflies ===
+  struct firefly {
+    // Position (track + phase)
+    const track *tr;
+    float t;
+    inline vec2 pos() const { return tr->at(t); }
+    float v;    // Velocity
+    bool sel;   // Selected?
+
+    firefly(const track *tr, float t, float v)
+      : tr(tr), t(t), v(v),
+        sel(false)
+      { }
+
+    inline void update(const std::vector<track *> &tracks) {
+      t += 1.0 / 240;
+      if (t >= tr->len) t -= tr->len;
+    }
+    inline void draw() const {
+      using namespace rl;
+      DrawCircleV(scr(pos()), 4, sel ? RED : YELLOW);
     }
   };
 
@@ -112,7 +131,7 @@ public:
       if (!last_on && on) c--;
       last_on = on;
     }
-    virtual void update(const std::vector<firefly *> &fireflies) = 0;
+    virtual void update(const std::vector<firefly> &fireflies) = 0;
     virtual void draw() const = 0;
   };
 
@@ -120,10 +139,10 @@ public:
     bellflower_ord(vec2 o, float r, int c0)
       : bellflower(o, r, c0)
       { }
-    void update(const std::vector<firefly *> &fireflies) {
+    void update(const std::vector<firefly> &fireflies) {
       bool on = false;
       for (const auto f : fireflies)
-        if ((f->at() - o).norm() <= r) {
+        if ((f.pos() - o).norm() <= r) {
           on = true;
           break;
         }
@@ -140,95 +159,108 @@ public:
   };
 
   // ==== Scene ====
-  std::vector<firefly *> fireflies;
+  std::vector<track *> tracks;
+  std::vector<firefly> fireflies, fireflies_init;
   std::vector<bellflower *> bellflowers;
 
-  scene_game() {
-    fireflies.push_back(new firefly_cir(vec2(4, 3), 2, 1, 0));
-    fireflies.push_back(new firefly_cir(vec2(6, 5), 3, 1, 0.25));
+  firefly *sel_ff;
+  track *sel_track;
+  vec2 sel_offs;
+  int run_state = 0;
+
+  scene_game()
+    : sel_ff(nullptr), sel_track(nullptr)
+  {
+    tracks.push_back(new track_cir(vec2(4, 3), 2));
+    tracks.push_back(new track_cir(vec2(6, 5), 3));
+    fireflies.push_back(firefly(tracks[0], 0, 1));
+    fireflies.push_back(firefly(tracks[1], 0.25, 1));
     bellflowers.push_back(new bellflower_ord(vec2(5, 7), 2, 4));
   }
 
-  firefly *sel_ff;
-  vec2 sel_offs;
-  int run_state = 0;
+  inline std::pair<firefly *, track *> find(const vec2 p) {
+    // Find the nearest firefly
+    firefly *best_ff = nullptr;
+    float best_dist = 0.75;
+    for (auto &f : fireflies) {
+      float dist = (p - f.pos()).norm();
+      if (dist < best_dist) {
+        best_dist = dist;
+        best_ff = &f;
+      }
+    }
+    if (best_ff != nullptr) return {best_ff, nullptr};
+
+    // Find the nearest firefly track
+    track *best_track = nullptr;
+    std::pair<float, float> best_result = {0, 0.5};
+    for (const auto t : tracks) {
+      auto result = t->nearest(p);
+      if (result.second < best_result.second) {
+        best_result = result;
+        best_track = t;
+      }
+    }
+    return {nullptr, best_track};
+  }
 
   void pton(float x, float y) {
     if (run_state & 1) return;
     vec2 p = board(x, y);
-    firefly *bestf = nullptr;
 
-    // Find the nearest firefly point
-    float bestpt = 0.75;
-    for (const auto f : fireflies) {
-      float dist = (p - f->at()).norm();
-      if (dist < bestpt) {
-        bestpt = dist;
-        bestf = f;
-      }
+    auto near = find(p);
+    if (near.first != nullptr) {
+      sel_ff = near.first;
+      sel_ff->sel = true;
+      sel_offs = sel_ff->pos() - p;
     }
-    if (bestf != nullptr) {
-      sel_ff = bestf;
-      sel_ff->selpt = true;
-      sel_offs = sel_ff->at() - p;
-      return;
-    }
-
-    // Find the nearest firefly path
-    std::pair<float, float> bestpath = {0, 0.5};
-    for (const auto f : fireflies) {
-      auto result = f->nearest(p);
-      if (result.second < bestpath.second) {
-        bestpath = result;
-        bestf = f;
-      }
-    }
-    if (bestf != nullptr) {
-      sel_ff = bestf;
-      sel_ff->selpath = true;
-      sel_offs = sel_ff->o - p;
+    if (near.second != nullptr) {
+      sel_track = near.second;
+      sel_track->sel = true;
+      sel_offs = sel_track->o - p;
     }
   }
 
   void ptmove(float x, float y) {
     vec2 p = board(x, y);
     if (sel_ff != nullptr) {
-      if (sel_ff->selpath) {
-        sel_ff->o = p + sel_offs;
-      } else {
-        sel_ff->tc = sel_ff->nearest(p + sel_offs).first;
-      }
+      sel_ff->t = sel_ff->tr->nearest(p + sel_offs).first;
+    }
+    if (sel_track != nullptr) {
+      sel_track->o = p + sel_offs;
     }
   }
 
   void ptoff(float x, float y) {
     if (sel_ff != nullptr) {
-      sel_ff->selpath = sel_ff->selpt = false;
+      sel_ff->sel = false;
       sel_ff = nullptr;
+    }
+    if (sel_track != nullptr) {
+      sel_track->sel = false;
+      sel_track = nullptr;
     }
   }
 
   inline void start_run() {
-    for (auto f : fireflies) f->tc0 = f->tc;
+    // Save
+    fireflies_init = fireflies;
   }
   inline void stop_run() {
-    for (auto f : fireflies) f->tc = f->tc0;
+    fireflies = fireflies_init;
     for (auto b : bellflowers) b->reset();
   }
 
   bool last_space_down = false;
   void update() {
     bool space_down = rl::IsKeyDown(rl::KEY_SPACE);
-    if (sel_ff == nullptr && !last_space_down && space_down) {
+    if (sel_track == nullptr && !last_space_down && space_down) {
       run_state ^= 1;
       if (run_state & 1) start_run(); else stop_run();
     }
     last_space_down = space_down;
     if (run_state & 1) {
-      for (auto f : fireflies) {
-        if ((f->tc += 1 / (240 * f->T)) >= 1)
-          f->tc -= 1;
-      }
+      for (auto &f : fireflies) f.update(tracks);
       for (auto b : bellflowers) b->update(fireflies);
     }
   }
@@ -240,8 +272,9 @@ public:
       DrawLineV(scr(vec2(i, 0)), scr(vec2(i, BOARD_H)), (Color){48, 48, 48, 255});
     for (int i = 0; i <= BOARD_H; i++)
       DrawLineV(scr(vec2(0, i)), scr(vec2(BOARD_W, i)), (Color){48, 48, 48, 255});
-    for (const auto f : fireflies) f->draw();
+    for (const auto t : tracks) t->draw();
     for (const auto b : bellflowers) b->draw();
+    for (const auto &f : fireflies) f.draw();
   }
 };
 
