bd7c224ee9d48982675767d8b8ee1f7850a771f9
Shiqing <shiqing-thu18@yandex.com>
2022-01-28 12:38:44 +0800

    Update Makefile for WASM

--- Makefile
+++ Makefile
@@ -1,5 +1,20 @@
-# For web:
-# make CXX=emcc LDFLAGS=deps/raylib/build_web/raylib/libraylib.a EXTRAFLAGS="-O2 -o game.html -s USE_GLFW=3 --preload-file res --shell-file deps/raylib/src/minshell.html -s ASYNCIFY -DPLATFORM_WEB"
+# mkdir build; cd build; cmake .. -DCMAKE_BUILD_TYPE=Release
+# emcmake cmake -S . -B build_wasm -DCMAKE_BUILD_TYPE=Release -DPLATFORM=Web
+
+TARGET = main
+
+ifeq ($(WEB),1)
+  CXX = emcc
+  RAYLIB_LIB ?= ./deps/raylib/build_wasm/raylib/libraylib.a
+  TARGET = web/index.html
+  EXTRAFLAGS ?= -O3 -o $(TARGET) -s USE_GLFW=3 --preload-file res --shell-file deps/raylib/src/minshell.html -DPLATFORM_WEB
+  EXTRASTEP = $(MD) web 2>/dev/null
+endif
+
+ifeq ($(shell uname),Darwin)
+  EXTRAFLAGS ?= -framework Cocoa -framework OpenGL -framework IOKit
+endif
+
 RAYLIB_LIB ?= ./deps/raylib/build/raylib/libraylib.a
 RAYLIB_INC ?= ./deps/raylib/src
 RM ?= rm
@@ -7,12 +22,13 @@ MD ?= mkdir
 DENO ?= deno
 
 CXXFLAGS := -I$(RAYLIB_INC) -I. -std=c++11
-LDFLAGS := $(RAYLIB_LIB) -framework Cocoa -framework OpenGL -framework IOKit
+LDFLAGS := $(RAYLIB_LIB)
 
 SOURCES := $(wildcard *.cc)
 HEADERS := $(wildcard *.hh)
 
-main: $(SOURCES) $(HEADERS)
+$(TARGET): $(SOURCES) $(HEADERS)
+	-$(EXTRASTEP)
 	$(CXX) -o $@ $(SOURCES) $(CXXFLAGS) $(LDFLAGS) $(EXTRAFLAGS)
 
 clean:
--- main.cc
+++ main.cc
@@ -1,7 +1,52 @@
 #include "main.hh"
 using namespace rl;
 
+#ifdef PLATFORM_WEB
+  #include <emscripten/emscripten.h>
+#endif
+
 #include <cmath>
+
+static scene *cur_scene;
+static bool pt_laston = false;
+static float pt_lastx, pt_lasty;
+
+static float time = 0;
+const float STEP = 1.0f / 240;
+
+static void update_draw_frame()
+{
+  BeginDrawing();
+
+  // Mouse
+  bool pt_on = IsMouseButtonDown(MOUSE_BUTTON_LEFT);
+  Vector2 pt_pos = GetMousePosition();
+  if (!pt_laston && pt_on) {
+    cur_scene->pton(pt_pos.x, pt_pos.y);
+    pt_lastx = pt_lasty = nanf("");
+  }
+  if (pt_on && (pt_pos.x != pt_lastx || pt_pos.y != pt_lasty)) {
+    cur_scene->ptmove(pt_pos.x, pt_pos.y);
+    pt_lastx = pt_pos.x;
+    pt_lasty = pt_pos.y;
+  }
+  if (pt_laston && !pt_on) {
+    cur_scene->ptoff(pt_pos.x, pt_pos.y);
+  }
+  pt_laston = pt_on;
+
+  // Update
+  time += GetFrameTime();
+  while (time >= STEP) {
+    time -= STEP;
+    cur_scene->update();
+  }
+
+  // Draw
+  cur_scene->draw();
+
+  EndDrawing();
+}
 
 int main(int argc, char *argv[])
 {
@@ -9,45 +54,14 @@ int main(int argc, char *argv[])
   InitWindow(W, H, NULL);
   SetTargetFPS(60);
 
-  scene *cur_scene = scene_game(99);
-  bool pt_laston = false;
-  float pt_lastx, pt_lasty;
+  cur_scene = scene_game(99);
 
-  float time = 0;
-  float STEP = 1.0f / 240;
-
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-
-    // Mouse
-    bool pt_on = IsMouseButtonDown(MOUSE_BUTTON_LEFT);
-    Vector2 pt_pos = GetMousePosition();
-    if (!pt_laston && pt_on) {
-      cur_scene->pton(pt_pos.x, pt_pos.y);
-      pt_lastx = pt_lasty = nanf("");
-    }
-    if (pt_on && (pt_pos.x != pt_lastx || pt_pos.y != pt_lasty)) {
-      cur_scene->ptmove(pt_pos.x, pt_pos.y);
-      pt_lastx = pt_pos.x;
-      pt_lasty = pt_pos.y;
-    }
-    if (pt_laston && !pt_on) {
-      cur_scene->ptoff(pt_pos.x, pt_pos.y);
-    }
-    pt_laston = pt_on;
-
-    // Update
-    time += GetFrameTime();
-    while (time >= STEP) {
-      time -= STEP;
-      cur_scene->update();
-    }
-
-    // Draw
-    cur_scene->draw();
-
-    EndDrawing();
-  }
+#ifdef PLATFORM_WEB
+  emscripten_set_main_loop(update_draw_frame, 0, 1);
+#else
+  while (!WindowShouldClose())
+    update_draw_frame();
+#endif
 
   CloseWindow();
 
